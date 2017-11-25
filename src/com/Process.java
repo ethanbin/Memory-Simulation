62a232a72c0dd0fc54c33f629f99e103040617e9
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2017-11-24 21:26:03 -0500

    Update Process.java
    The constructor has four parameters because that's how it should be read in from a file.

--- src/com/Process.java
+++ src/com/Process.java
@@ -5,46 +5,58 @@ import java.util.*;
 public class Process {
 
 	private String name;
+	private int arrivalTime;
 	private int size;
+	private int finishTime;
 
 	public Process() {
 		name = "";
 		size = 0;
 	}
 
-	public Process(String name, int size) {
+	public Process(String name, int arrivalTime, int size, int finishTime) {
 		this.name = name;
+		this.arrivalTime = arrivalTime;
 		this.size = size;
+		this.finishTime = finishTime;
 	}
 
 	public String getName() {
 		return name;
 	}
 
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+	
 	public int getSize() {
 		return size;
 	}
 	
-	
+	private int getFinishTime() {
+		return finishTime;
+	}
+
 	@Override
 	public String toString() {
-		return "Process [name=" + name + ", size=" + size + "]";
+		return "Process [name=" + name + ", arrivalTime=" + arrivalTime + ", size=" + size + ", finishTime="
+				+ finishTime + "]";
 	}
 
 	public static void main(String[]args) {
 		ArrayList<Process> testing = new ArrayList<Process>();
-		testing.add(new Process("A", 100));
-		testing.add(new Process("B", 120));
-		testing.add(new Process("C", 140));
-		testing.add(new Process("D", 160));
-		testing.add(new Process("E", 180));
+		testing.add(new Process("A", 0, 131, 4));
+		testing.add(new Process("B", 2, 120, 4));
+		testing.add(new Process("C", 5, 58, 7));
+		testing.add(new Process("D", 9, 107, 5));
+		testing.add(new Process("E", 13, 82, 3));
 		
 		System.out.println(testing);
 			
 		testing.remove(2);							//can remove from anywhere
 		System.out.println(testing);		
 		
-		testing.add(2, new Process("C", 140));		//can specify where to add
+		testing.add(2, new Process("C", 5, 140, 7));		//can specify where to add
 		System.out.println(testing);
 		
 		System.out.println(testing.get(4));			//can specify which particular element you want
@@ -52,4 +64,5 @@ public class Process {
 		System.out.println(testing.size());			//how many elements are in the list
 		
 	}
+
 }
