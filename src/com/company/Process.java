34f9bf9079b6f48f41822316123e8913d202ada0
Ethan <ethanbinya@aol.com>
2017-11-27 12:06:35 -0500

    Process now extends MemoryAllocation. Removed int property size and usememorySize from MemoryAllocation instead.

--- src/com/company/Process.java
+++ src/com/company/Process.java
@@ -2,50 +2,61 @@ package com.company;
 
 import java.util.*;
 
-public class Process {
+public class Process extends  MemoryAllocation{
 
 	private String name;
 	private int arrivalTime;
-	private int size;
 	private int finishTime;
 
 	public Process() {
-		name = "";
-		arrivalTime = 0;
-		size = 0;
-		finishTime = 0;
-	}
+        init("", 0, 0, 0);
+    }
 
 	public Process(String name, int arrivalTime, int size, int finishTime) {
-		this.name = name;
-		this.arrivalTime = arrivalTime;
-		this.size = size;
-		this.finishTime = finishTime;
+		init(name, arrivalTime, size, finishTime);
 	}
+
+    public void init(String name, int arrivalTime, int size, int finishTime) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.memorySize = size;
+        this.finishTime = finishTime;
+    }
 
 	public String getName() {
 		return name;
 	}
 
-	public int getArrivalTime() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getArrivalTime() {
 		return arrivalTime;
 	}
-	
-	public int getSize() {
-		return size;
-	}
-	
-	public int getFinishTime() {
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setFinishTime(int finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public int getFinishTime() {
 		return finishTime;
 	}
 
-	@Override
-	public String toString() {
-		return "Process [name=" + name + ", arrivalTime=" + arrivalTime + ", size=" + size + ", finishTime="
-				+ finishTime + "]";
-	}
+    @Override
+    public String toString() {
+        return "Process{" +
+                "name='" + name + '\'' +
+                ", arrivalTime=" + arrivalTime +
+                ", finishTime=" + finishTime +
+                "} " + super.toString();
+    }
 
-	public static void main(String[]args) {
+    public static void main(String[]args) {
 		ArrayList<Process> testing = new ArrayList<Process>();
 		testing.add(new Process("A", 0, 131, 4));
 		testing.add(new Process("B", 2, 120, 4));
@@ -64,7 +75,12 @@ public class Process {
 		System.out.println(testing.get(4));			//can specify which particular element you want
 		
 		System.out.println(testing.size());			//how many elements are in the list
-		
+
+		List<MemoryAllocation> memory = new ArrayList<>();
+		memory.add(new MemoryAllocation());
+		memory.add(new Process("E", 13, 82, 3));
+		Process p = (Process) memory.get(1);
+		System.out.println(p);
 	}
 
 }
