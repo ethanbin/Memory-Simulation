a3327390c955eecb49fda21cda6a46d600874976
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2017-11-28 14:58:20 -0500

    MemorySize is now MemorySize NeededMemorySizeUsed is added

--- src/com/company/Memory.java
+++ src/com/company/Memory.java
@@ -7,27 +7,30 @@ import java.util.List;
  * Created by Ethan on 11/27/2017.
  */
 public abstract class Memory {
-    protected final int MEMORY_SIZE = 1024;
+    protected final int DEFAULT_MEMORY_SIZE = 1024;
     protected static List<MemoryAllocation> memoryList;
     private int totalMemorySize = 0;					//space that has been allocated
     
+    protected int size;
     protected int internalFragmenation = 0;
     protected int externalFragmentation = 0;
-    protected int currTime = 0;							// keeps track of time
-    protected int partitionCount = 0;
-    protected int allocationFail = 0;
+	protected int currTime = 0;							// keeps track of time
+	protected int partitionCount = 0;
+	protected int allocationFail = 0;
 
     public Memory(){
-        init(MEMORY_SIZE);
+    	size = DEFAULT_MEMORY_SIZE;
+        init(size);
     }
 
     public Memory(int size){
+    	this.size = size;
         init(size);
     }
 
     protected void init(int size){
         memoryList = new ArrayList<>();
-        memoryList.add(new MemoryAllocation(size, 0, MEMORY_SIZE));
+        memoryList.add(new MemoryAllocation(size, 0, DEFAULT_MEMORY_SIZE));
     }
 
     public abstract void addProcess(Process p, int pos);
@@ -41,7 +44,7 @@ public abstract class Memory {
     @Override
     public String toString() {
         return "Memory{" +
-                "DEFAULT_MEMORY_SIZE=" + MEMORY_SIZE +
+                "Size=" + size +
                 ", memoryList=" + memoryList +
                 ", totalMemorySize=" + totalMemorySize +
                 '}';
