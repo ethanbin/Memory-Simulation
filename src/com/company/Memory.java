e4d97a2560309e8f4fbe169c89cae38d179c9087
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2017-11-28 14:20:41 -0500

    Memory is now an abstract class

--- src/com/company/Memory.java
+++ src/com/company/Memory.java
@@ -6,13 +6,20 @@ import java.util.List;
 /**
  * Created by Ethan on 11/27/2017.
  */
-public class Memory {
-    protected final int DEFAULT_MEMORY_SIZE = 1024;
-    protected List<MemoryAllocation> memoryList;
-    int totalMemorySize;
+public abstract class Memory {
+    protected final int MEMORY_SIZE = 1024;
+    protected int PARTITIONSIZE = 128;					//for equal sized fixed partition
+    protected static List<MemoryAllocation> memoryList;
+    private int totalMemorySize = 0;					//space that has been allocated
+    
+    protected int internalFragmenation = 0;
+    protected int externalFragmentation = 0;
+	protected int currTime = 0;							// keeps track of time
+	protected int partitionCount = 0;
+	protected int allocationFail = 0;
 
     public Memory(){
-        init(DEFAULT_MEMORY_SIZE);
+        init(MEMORY_SIZE);
     }
 
     public Memory(int size){
@@ -21,13 +28,21 @@ public class Memory {
 
     protected void init(int size){
         memoryList = new ArrayList<>();
-        memoryList.add(new MemoryAllocation(size, 0, DEFAULT_MEMORY_SIZE));
+        memoryList.add(new MemoryAllocation(size, 0, MEMORY_SIZE));
     }
 
+    public abstract void addProcess(Process p, int pos);
+    
+    public abstract void removeProcess(int pos);
+    
+    public abstract void calculateInternalFragmentation(Process p);
+    
+    public abstract void calculateExternalFragmentation(Process P);
+    
     @Override
     public String toString() {
         return "Memory{" +
-                "DEFAULT_MEMORY_SIZE=" + DEFAULT_MEMORY_SIZE +
+                "DEFAULT_MEMORY_SIZE=" + MEMORY_SIZE +
                 ", memoryList=" + memoryList +
                 ", totalMemorySize=" + totalMemorySize +
                 '}';
