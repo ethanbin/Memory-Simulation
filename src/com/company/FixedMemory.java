421983a15eca15db09c715be004fee54f7c0bff7
Ethan <ethanbinya@aol.com>
2017-11-29 17:34:29 -0500

    Defined removeProcess methods.

--- src/com/company/FixedMemory.java
+++ src/com/company/FixedMemory.java
@@ -13,6 +13,8 @@ public abstract class FixedMemory extends Memory {
                 continue;
             if (currentAllocation.getMemorySizeUsed() <= p.getMemorySizeNeeded()) {
                 p.setMemorySizeUsed(currentAllocation.getMemorySizeUsed());
+                p.setEndingPositionInMemory(currentAllocation.getEndingPositionInMemory());
+                p.setStartingPositionInMemory(currentAllocation.getStartingPositionInMemory());
                 memoryList.set(i, p);
                 return true;
             }
@@ -22,11 +24,35 @@ public abstract class FixedMemory extends Memory {
 
     @Override
     public boolean removeProcess(String processName) {
+        for (int i = 0; i < memoryList.size(); i++){
+            if (!isMemoryAllocationAProcess(memoryList.get(i)))
+                continue;
+            Process currentProcess = (Process) memoryList.get(i);
+            if (!currentProcess.getName().equals(processName))
+                continue;
+            MemoryAllocation memAlloc = new MemoryAllocation(currentProcess.getMemorySizeUsed(),
+                    currentProcess.getStartingPositionInMemory(),
+                    currentProcess.getEndingPositionInMemory());
+            memoryList.set(i, memAlloc);
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean removeProcess(int startingPositionInMemory) {
+        for (int i = 0; i < memoryList.size(); i++){
+            if (!isMemoryAllocationAProcess(memoryList.get(i)))
+                continue;
+            Process currentProcess = (Process) memoryList.get(i);
+            if (currentProcess.getStartingPositionInMemory() != startingPositionInMemory)
+                continue;
+            MemoryAllocation memAlloc = new MemoryAllocation(currentProcess.getMemorySizeUsed(),
+                    currentProcess.getStartingPositionInMemory(),
+                    currentProcess.getEndingPositionInMemory());
+            memoryList.set(i, memAlloc);
+            return true;
+        }
         return false;
     }
 }
