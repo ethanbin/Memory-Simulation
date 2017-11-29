3a8b1e48a164a42e40a8362e472cca8268ad0f09
Ethan <ethanbinya@aol.com>
2017-11-29 15:29:43 -0500

    Added DynamicMemory, FixedMemory, and Compactable.

---
+++ src/com/company/DynamicMemory.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by Ethan on 11/29/2017.
+ */
+public class DynamicMemory extends Memory implements Compactable{
+}
---
+++ src/com/company/FixedMemory.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by Ethan on 11/29/2017.
+ */
+public class FixedMemory extends Memory {
+}
