658c3ce82b769d168d3bad41d8749c2adf25a0fa
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2017-12-03 18:03:26 -0500

    What I think needs to be here

--- src/com/company/FixedMemory.java
+++ src/com/company/FixedMemory.java
@@ -4,55 +4,19 @@ package com.company;
  * Created by Ethan on 11/29/2017.
  */
 public abstract class FixedMemory extends Memory {
-    @Override
-    public boolean addProcess(Process p) {
-        for (int i = 0; i < memoryList.size(); i++){
-            MemoryAllocation currentAllocation = memoryList.get(i);
-            // if current place isn't free space, continue loop to next place
-            if (isMemoryAllocationAProcess(currentAllocation))
-                continue;
-            if (currentAllocation.getMemorySizeUsed() <= p.getMemorySizeNeeded()) {
-                p.setMemorySizeUsed(currentAllocation.getMemorySizeUsed());
-                p.setEndingPositionInMemory(currentAllocation.getEndingPositionInMemory());
-                p.setStartingPositionInMemory(currentAllocation.getStartingPositionInMemory());
-                memoryList.set(i, p);
-                return true;
-            }
-        }
-        return false;
-    }
+	
+	@Override
+	public void calculateInternalFragmentation(int partitionSize, Process p) {
+		internalFragmentation += partitionSize - p.getMemorySizeNeeded();
+	}
 
-    @Override
-    public boolean removeProcess(String processName) {
-        for (int i = 0; i < memoryList.size(); i++){
-            if (!isMemoryAllocationAProcess(memoryList.get(i)))
-                continue;
-            Process currentProcess = (Process) memoryList.get(i);
-            if (!currentProcess.getName().equals(processName))
-                continue;
-            MemoryAllocation memAlloc = new MemoryAllocation(currentProcess.getMemorySizeUsed(),
-                    currentProcess.getStartingPositionInMemory(),
-                    currentProcess.getEndingPositionInMemory());
-            memoryList.set(i, memAlloc);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean removeProcess(int startingPositionInMemory) {
-        for (int i = 0; i < memoryList.size(); i++){
-            if (!isMemoryAllocationAProcess(memoryList.get(i)))
-                continue;
-            Process currentProcess = (Process) memoryList.get(i);
-            if (currentProcess.getStartingPositionInMemory() != startingPositionInMemory)
-                continue;
-            MemoryAllocation memAlloc = new MemoryAllocation(currentProcess.getMemorySizeUsed(),
-                    currentProcess.getStartingPositionInMemory(),
-                    currentProcess.getEndingPositionInMemory());
-            memoryList.set(i, memAlloc);
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public void calculateExternalFragmentation(Process P) {
+		// fixed sized partitions don't have external fragmentation
+	}
+	
+	@Override
+	public void removeFinishedProcesses() {
+		
+	}
 }
