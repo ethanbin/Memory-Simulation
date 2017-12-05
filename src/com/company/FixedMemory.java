4b8096d739441b7c38baa36b8bcc3e01cd311ca9
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2017-12-05 18:32:59 -0500

    both methods properly work

--- src/com/company/FixedMemory.java
+++ src/com/company/FixedMemory.java
@@ -6,21 +6,43 @@ package com.company;
 public abstract class FixedMemory extends Memory {
 	
 	@Override
-	public void calculateInternalFragmentation(int partitionSize, Process p) {
-		internalFragmentation += partitionSize - p.getMemorySizeNeeded();
+	public boolean addProcesses(Process p, int partition) {
+		for (int i = 0; i < memoryList.size(); i++) {
+			MemoryAllocation currentAllocation = memoryList.get(i);
+
+			// if current place isn't free space, continue loop to next place
+			if (isMemoryAllocationAProcess(currentAllocation))
+				continue;
+
+			// checking to make sure the process can fit
+			if (!p.isHasBeenAllocated()) {
+				p.setMemorySizeUsed(p.getMemorySizeNeeded()); // set the size we will be taking up
+
+				p.setStartingPositionInMemory(currMemoryPos); // the last stored time
+				currMemoryPos += p.getMemorySizeNeeded();
+				p.setEndingPositionInMemory(currMemoryPos);
+				currMemoryPos = 128 * (partition + 1);
+
+				p.setProcessFinishTime(p.getArrivalTime() + p.getTimeofProcess());
+				currentTime = p.getArrivalTime();
+				calculateInternalFragmentation(p);
+				memoryList.set(i, p);
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
-	public void calculateExternalFragmentation(Process P) {
-		// fixed sized partitions don't have external fragmentation
-	}
-	
-	@Override
-	public void removeFinishedProcesses(int currTime, int totalPartitions) {
-		for(int i = 0; i < totalPartitions; i++) {
-			Process currProcess = (Process)memoryList.get(i);
-			if(currProcess.getFinishTime() <= currTime) 	//if it should be done, remove it
-				memoryList.remove(i);
+	public void removeProcesses() {
+		for (int i = 0; i < memoryList.size(); i++) {
+			Process currentProcess = (Process) memoryList.get(i);
+			
+			if (currentProcess.getProcessFinishTime() <= currentTime) {
+				// calculateInternalFragmentation(currentProcess);
+				MemoryAllocation memAlloc = new MemoryAllocation();
+				memoryList.set(i, memAlloc);
+			}
 		}
 	}
 }
