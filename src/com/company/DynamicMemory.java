e12f204ea4e6b1cb427afaa37df36ec75ddb6231
Ethan <ethanbinya@aol.com>
2017-12-06 13:48:51 -0500

    Forgot to instantiate memoryList - done now.

--- src/com/company/DynamicMemory.java
+++ src/com/company/DynamicMemory.java
@@ -4,6 +4,9 @@ import com.company.ProcessInserter.BestFitProcessInserter;
 import com.company.ProcessInserter.FirstFitProcessInserter;
 import com.company.ProcessInserter.ProcessInserter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Ethan on 11/29/2017.
  */
@@ -22,6 +25,7 @@ public class DynamicMemory extends Memory implements Compactable{
 
     @Override
     protected void init(int size) {
+        memoryList = new ArrayList<>();
         memoryList.add(new MemoryAllocation(size, 0, size - 1));
     }
 
@@ -74,6 +78,23 @@ public class DynamicMemory extends Memory implements Compactable{
     }
 
     public static void main(String[] args) {
+        DynamicMemory firstFit = new DynamicMemory(new FirstFitProcessInserter());
+        List<Process> jobList = new ArrayList<>();
+        jobList.add(new Process("A", 0, 131, 4));
+        jobList.add(new Process("B", 2, 120, 4));
+        jobList.add(new Process("C", 5, 158, 7));
+        jobList.add(new Process("D", 9, 107, 5));
+        jobList.add(new Process("E", 13, 82, 3));
+        jobList.add(new Process("F", 17, 127, 1));
+        jobList.add(new Process("G", 17, 43, 8));
+        jobList.add(new Process("H", 20, 77, 6));
+        jobList.add(new Process("I", 24, 109, 2));
+        jobList.add(new Process("J", 26, 90, 3));
+        jobList.add(new Process("K", 29, 190, 7));
+        jobList.add(new Process("L", 31, 24, 2));
 
+        for (Process p : jobList)
+            firstFit.addProcess(p);
+        System.out.println(firstFit);
     }
 }
