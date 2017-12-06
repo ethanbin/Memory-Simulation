c38fae708dbb41469d307640a63e2a4b7a278ae7
Ethan <ethanbinya@aol.com>
2017-12-06 11:40:07 -0500

    Added overrided method, empty/unfinished.

--- src/com/company/ProcessInserter/BestFitProcessInserter.java
+++ src/com/company/ProcessInserter/BestFitProcessInserter.java
@@ -1,7 +1,15 @@
 package com.company.ProcessInserter;
 
+import com.company.MemoryAllocation;
+import com.company.Process;
+import java.util.List;
+
 /**
  * Created by Ethan on 12/6/2017.
  */
-public class BestFitProcessInserter {
+public class BestFitProcessInserter extends ProcessInserter{
+    @Override
+    public boolean tryAllocatingProcess(Process proc, List<MemoryAllocation> list){
+        return true;
+    }
 }
--- src/com/company/ProcessInserter/WorstFitProcessInserter.java
+++ src/com/company/ProcessInserter/WorstFitProcessInserter.java
@@ -1,7 +1,15 @@
 package com.company.ProcessInserter;
 
+import com.company.MemoryAllocation;
+import com.company.Process;
+import java.util.List;
+
 /**
  * Created by Ethan on 12/6/2017.
  */
-public class WorstFitProcessInserter {
+public class WorstFitProcessInserter extends ProcessInserter{
+    @Override
+    public boolean tryAllocatingProcess(Process proc, List<MemoryAllocation> list){
+        return true;
+    }
 }
