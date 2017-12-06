90b44f343d29cc4254f0cab56a9361b0e7dca236
Ethan <ethanbinya@aol.com>
2017-12-06 09:55:26 -0500

    Defined tryAllocatingProcess for FirstFit.

--- src/com/company/ProcessInserter/FirstFitProcessInserter.java
+++ src/com/company/ProcessInserter/FirstFitProcessInserter.java
@@ -1,7 +1,20 @@
 package com.company.ProcessInserter;
 
+import com.company.MemoryAllocation;
+import com.company.Process;
+import java.util.List;
+
 /**
  * Created by Ethan on 12/6/2017.
  */
-public class FirstFitProcessInserter {
+public class FirstFitProcessInserter extends ProcessInserter{
+    @Override
+    public boolean tryAllocatingProcess(Process proc, List<MemoryAllocation> list){
+        for (int i = 0; i < list.size(); i++){
+            if (isMemoryAllocationAProcessOrTooSmall(list.get(i), proc.getMemorySizeNeeded()))
+                continue;
+            allocateProcessToIndex(proc, i, list);
+        }
+        return false;
+    }
 }
