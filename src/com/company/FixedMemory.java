f2c02dd92ae3b8fcf3a230d05fa5d4567acd8779
Ethan <ethanbinya@aol.com>
2017-12-06 19:39:35 -0500

    Copied addProcess for FixedMemory from the Kailie branch.

--- src/com/company/FixedMemory.java
+++ src/com/company/FixedMemory.java
@@ -1,56 +1,25 @@
 package com.company;
 
-/**
- * Created by Ethan on 11/29/2017.
- */
 public abstract class FixedMemory extends Memory {
+
     @Override
-    public boolean addProcess(Process p) {
-        for (int i = 0; i < memoryList.size(); i++){
+    public boolean addProcess(Process proc) {
+        for (int i = 0; i < memoryList.size(); i++) {
             MemoryAllocation currentAllocation = memoryList.get(i);
+
             // if current place isn't free space, continue loop to next place
             if (isMemoryAllocationAProcess(currentAllocation))
                 continue;
-            if (currentAllocation.getMemorySizeUsed() >= p.getMemorySizeNeeded()) {
-                p.setMemorySizeUsed(currentAllocation.getMemorySizeUsed());
-                p.setEndingPositionInMemory(currentAllocation.getEndingPositionInMemory());
-                p.setStartingPositionInMemory(currentAllocation.getStartingPositionInMemory());
-                memoryList.set(i, p);
-                return true;
-            }
-        }
-        return false;
-    }
 
-    @Override
-    public boolean removeProcess(String processName) {
-        for (int i = 0; i < memoryList.size(); i++){
-            if (!isMemoryAllocationAProcess(memoryList.get(i)))
-                continue;
-            Process currentProcess = (Process) memoryList.get(i);
-            if (!currentProcess.getName().equals(processName))
-                continue;
-            MemoryAllocation memAlloc = new MemoryAllocation(currentProcess.getMemorySizeUsed(),
-                    currentProcess.getStartingPositionInMemory(),
-                    currentProcess.getEndingPositionInMemory());
-            memoryList.set(i, memAlloc);
-            return true;
-        }
-        return false;
-    }
+            proc.setMemorySizeUsed(currentAllocation.getMemorySizeUsed()); // set the size we will be taking up
 
-    @Override
-    public boolean removeProcess(int startingPositionInMemory) {
-        for (int i = 0; i < memoryList.size(); i++){
-            if (!isMemoryAllocationAProcess(memoryList.get(i)))
-                continue;
-            Process currentProcess = (Process) memoryList.get(i);
-            if (currentProcess.getStartingPositionInMemory() != startingPositionInMemory)
-                continue;
-            MemoryAllocation memAlloc = new MemoryAllocation(currentProcess.getMemorySizeUsed(),
-                    currentProcess.getStartingPositionInMemory(),
-                    currentProcess.getEndingPositionInMemory());
-            memoryList.set(i, memAlloc);
+            proc.setStartingPositionInMemory(memoryList.get(i).getStartingPositionInMemory());
+            proc.setEndingPositionInMemory(memoryList.get(i).getEndingPositionInMemory());
+
+            proc.setFinishTime(proc.getArrivalTime() + proc.getRunTime());
+            currentTime = proc.getArrivalTime();
+            calculateInternalFragmentation();
+            memoryList.set(i, proc);
             return true;
         }
         return false;
