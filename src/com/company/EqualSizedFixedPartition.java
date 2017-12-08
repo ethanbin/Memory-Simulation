3b244b5e09814c2d3fcfe773dc902cb90fdd1986
Ethan <ethanbinya@aol.com>
2017-12-08 02:32:30 -0500

    Defined WorstFitProcessInserter's tryAllocatingProcess.

--- src/com/company/EqualSizedFixedPartition.java
+++ src/com/company/EqualSizedFixedPartition.java
@@ -24,16 +24,6 @@ public class EqualSizedFixedPartition extends FixedMemory {
         fragmentations = new ArrayList<>();
     }
 
-
-	public boolean isProcessDone(Process a) {
-		boolean done = false;
-		if (currentTime <= a.getArrivalTime() + a.getFinishTime()) { // if the process is done, we will release it
-			done = true;
-			a = null;
-		}
-		return done;
-	}
-
 	/*
 	public void removeProcess(int pos) {
 		memoryList.remove(pos);
@@ -46,28 +36,5 @@ public class EqualSizedFixedPartition extends FixedMemory {
 		System.out.println("Allocation fails: " + allocationFailures);
 		//System.out.println("Average Internal Fragmentation: " + internalFragmentation / jobList.size() + "MB");
 		//System.out.println("Average External Freagmentation: " + externalFragmentation / jobList.size() + "MB");
-	}
-
-	public static void main(String[] args) {
-		ArrayList<Process> jobList = new ArrayList<>();
-
-		jobList.add(new Process("A", 0, 131, 4));
-		jobList.add(new Process("B", 2, 120, 4));
-		jobList.add(new Process("C", 5, 158, 7));
-		jobList.add(new Process("D", 9, 107, 5));
-		jobList.add(new Process("E", 13, 82, 3));
-		jobList.add(new Process("F", 17, 127, 1));
-		jobList.add(new Process("G", 17, 43, 8));
-		jobList.add(new Process("H", 20, 77, 6));
-		jobList.add(new Process("I", 24, 109, 2));
-		jobList.add(new Process("J", 26, 90, 3));
-		jobList.add(new Process("K", 29, 190, 7));
-		jobList.add(new Process("L", 31, 24, 2));
-
-		EqualSizedFixedPartition a = new EqualSizedFixedPartition();
-		for (Process p : jobList)
-    		a.addProcess(p);
-		a.display();
-
 	}
 }
--- src/com/company/ProcessInserter/WorstFitProcessInserter.java
+++ src/com/company/ProcessInserter/WorstFitProcessInserter.java
@@ -10,6 +10,19 @@ import java.util.List;
 public class WorstFitProcessInserter extends ProcessInserter{
     @Override
     public boolean tryAllocatingProcess(Process proc, List<MemoryAllocation> list){
+        int largestSizedIndex = -1;
+        int largestMemorySize = -1;
+        for (int i = 0; i < list.size(); i++){
+            if (isMemoryAllocationAProcessOrTooSmall(list.get(i), proc.getMemorySizeNeeded()))
+                continue;
+            if (list.get(i).getMemorySizeUsed() > largestMemorySize) {
+                largestSizedIndex = i;
+                largestMemorySize = list.get(i).getMemorySizeUsed();
+            }
+        }
+        if (largestSizedIndex == -1)
+            return false;
+        allocateProcessToIndex(proc, largestSizedIndex, list);
         return true;
     }
 }
