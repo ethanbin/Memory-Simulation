70980d0f32d8a3f334a8057d862c230afe4a004b
Ethan <ethanbinya@aol.com>
2017-12-08 03:17:48 -0500

    Defined best fit inserter's overrided method.

--- src/com/company/ProcessInserter/BestFitProcessInserter.java
+++ src/com/company/ProcessInserter/BestFitProcessInserter.java
@@ -10,6 +10,19 @@ import java.util.List;
 public class BestFitProcessInserter extends ProcessInserter{
     @Override
     public boolean tryAllocatingProcess(Process proc, List<MemoryAllocation> list){
+        int largestFittingSizedIndex = -1;
+        int smallestMemorySizeDifference = -1;
+        for (int i = 0; i < list.size(); i++){
+            if (isMemoryAllocationAProcessOrTooSmall(list.get(i), proc.getMemorySizeNeeded()))
+                continue;
+            if (list.get(i).getMemorySizeUsed() - proc.getMemorySizeNeeded() < smallestMemorySizeDifference) {
+                largestFittingSizedIndex = i;
+                smallestMemorySizeDifference = list.get(i).getMemorySizeUsed() - proc.getMemorySizeNeeded();
+            }
+        }
+        if (largestFittingSizedIndex == -1)
+            return false;
+        allocateProcessToIndex(proc, largestFittingSizedIndex, list);
         return true;
     }
 }
