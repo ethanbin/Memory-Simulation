dfcfacd01eae70aa2d897a737db5da0eafc5e2de
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2017-12-10 00:38:50 -0500

    Update generateData.java

--- src/com/company/generateData.java
+++ src/com/company/generateData.java
@@ -5,31 +5,12 @@ import java.io.PrintWriter;
 import java.io.IOException;
 
 public class generateData {
-
-	public static void main(String[] args) throws IOException{
-		String file = "F:/testing.txt";
+	
+	public generateData(){
 		
-		try {
-			ReadFile a = new ReadFile(file);
-			String [] lines = a.OpenFile();
-			
-			
-			for(int i = 0; i < lines.length; i++) {
-				System.out.println(lines[i]);
-			}
-			
-			WriteFile b = new WriteFile(file, false);
-			b.write("This is a test.....");
-			
-			ReadFile c = new ReadFile(file);
-			String [] line = a.OpenFile();
-			
-			for(int i = 0; i < line.length; i++) {
-				System.out.println(line[i]);
-			}
-		}
-		catch (IOException e) {
-			System.out.print(e.getMessage());
-		}
+	}
+	
+	public generateData(int numberOfProcesses, int maxArrivalTime, int maxFinishTime){
+		
 	}
 }
