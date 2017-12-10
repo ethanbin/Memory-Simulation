4ca7ce5c7c35419d0fba1ffbe30fdb107c79d044
Ethan <ethanbinya@aol.com>
2017-12-10 18:45:14 -0500

    Added Process comparator to compare process numbers.

---
+++ src/ProcessNumberComparator.java
@@ -0,0 +1,15 @@
+import com.company.Process;
+
+import java.util.Comparator;
+
+/**
+ * Created by Ethan on 12/10/2017.
+ */
+public class ProcessNumberComparator implements Comparator<Process> {
+    @Override
+    public int compare(Process a, Process b) {
+        if (a.getProcessNumber() == b.getProcessNumber())
+            return 0;
+        return a.getProcessNumber() > b.getProcessNumber() ? 1 : -1;
+    }
+}
