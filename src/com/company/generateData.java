6b4424a8de1689b9556710a91ff22c6ce4d08235
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2017-12-10 00:59:46 -0500

    Update generateData.java

--- src/com/company/generateData.java
+++ src/com/company/generateData.java
@@ -1,16 +1,43 @@
 package com.company;
 
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.IOException;
+import java.util.*;
 
 public class generateData {
-	
-	public generateData(){
-		
+
+	private final static int DEFAULT_NUMBER_OF_PROCESSES = 20;
+	private final static int DEFAULT_MAX_ARRIVAL_TIME = 50;
+	private final static int DEFAULT_MAX_FINISH_TIME = 100;
+
+	private int numberOfProcesses; // based on the user input
+	private int maxArrivalTime;
+	private int maxFinishTime;
+
+	private int processNumber;
+	private int sizeOfSpace;
+	private int arrivalTime;
+	private int finishTime;
+
+	public generateData() {
+		numberOfProcesses = DEFAULT_NUMBER_OF_PROCESSES;
+		maxArrivalTime = DEFAULT_MAX_ARRIVAL_TIME;
+		maxFinishTime = DEFAULT_MAX_FINISH_TIME;
+		init();
 	}
-	
-	public generateData(int numberOfProcesses, int maxArrivalTime, int maxFinishTime){
-		
+
+	public generateData(int numberOfProcesses, int maxArrivalTime, int maxFinishTime) {
+		this.numberOfProcesses = numberOfProcesses;
+		this.maxArrivalTime = maxArrivalTime;
+		this.maxFinishTime = maxFinishTime;
+		init();
+	}
+
+	public void init() {
+		processNumber = 0;
+		arrivalTime = 0;
+		finishTime = arrivalTime + 1;
+	}
+
+	public static void main(String[] args) {
+
 	}
 }
